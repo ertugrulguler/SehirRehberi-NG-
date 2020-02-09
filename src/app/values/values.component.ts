29d79fa5356b5d409650a892a15b7e1fcc6aaeb4
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-09 22:01:34 +0300

    Values komponenti için basit tablo tasarımı yapıldı. App componentinde çağrıldı.

--- src/app/values/values.component.ts
+++ src/app/values/values.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Value } from "../models/Value";
 
 @Component({
-  selector: 'app-values',
-  templateUrl: './values.component.html',
-  styleUrls: ['./values.component.css']
+  selector: "app-values",
+  templateUrl: "./values.component.html",
+  styleUrls: ["./values.component.css"],
+  providers: [HttpClient]
 })
 export class ValuesComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
+  values: Value[] = [];
   ngOnInit() {
+    this.getValues().subscribe(response => {
+      this.values = response;
+    });
   }
 
+  getValues() {
+    return this.http.get<Value[]>("http://localhost:56669/api/values");
+  }
 }
