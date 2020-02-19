976fd7bd617a544392905c66f7ad6915c1729241
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-19 17:14:36 +0300

    Şehir ekleme komponenti routelara eklendi.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { CityComponent } from "./city/city.component";
 import { ValuesComponent } from "./values/values.component";
 import { CityDetailComponent } from "./city/city-detail/city-detail.component";
+import { CityAddComponent } from "./city/city-add/city-add.component";
 
 export const appRoutes: Routes = [
   { path: "city", component: CityComponent },
+  { path: "cityAdd", component: CityAddComponent },
   { path: "cityDetail/:id", component: CityDetailComponent },
   { path: "values", component: ValuesComponent },
   { path: "**", redirectTo: "city", pathMatch: "full" }
