3c18a931b482f0b86a67b283e095c69166376aa6
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-19 18:21:28 +0300

    Şehir ekleme navbara eklendi ve route işlemi sağlandı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./routes";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,7 +27,9 @@ import { CityAddComponent } from "./city/city-add/city-add.component";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule
     // NgxGalleryModule
   ],
   providers: [],
